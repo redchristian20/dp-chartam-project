d452cf6fcb278806712603314a28bcfdf86e82a7
Christian Balidoy <redchristian20@gmail.com>
2025-06-05 18:14:21 +0800

    .

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/redchristian20/dp-chartam-project",
   plugins: [react(),tailwindcss()],
 })
