718bd0150e6682490d29e1bf82fb0c7f018b7d1b
Christian Balidoy <redchristian20@gmail.com>
2025-06-13 01:17:59 +0800

    .

--- alchemy/src/components/Footer/Footer.jsx
+++ alchemy/src/components/Footer/Footer.jsx
@@ -24,8 +24,11 @@ const Footer = () => {
             </div>
             <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="#" class="hover:underline">Alchemy</a>. All Rights Reserved.</span>
+            {//test
+        }
         </div>
     </div>
+    
   )
 }
 
