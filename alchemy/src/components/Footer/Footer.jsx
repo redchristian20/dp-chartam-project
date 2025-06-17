ff93b010be2e01a18f9eafb6a23bea3388408e74
Christian Balidoy <redchristian20@gmail.com>
2025-06-17 22:59:55 +0800

    Update Footer.jsx

--- alchemy/src/components/Footer/Footer.jsx
+++ alchemy/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
             <div className="sm:flex sm:items-center sm:justify-between">
                 <a href="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                    <img src={logo} className="h-8" alt="Alchemy Logo" />
+                    <img src={logo} className="h-24 dark:invert" alt="Alchemy Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Alchemy</span>
                 </a>
                 <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -28,8 +28,6 @@ const Footer = () => {
         }
         </div>
     </div>
-    
   )
 }
-
 export default Footer
