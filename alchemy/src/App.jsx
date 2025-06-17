a85e6ba9f68459aa1cd57838059dd14e3284ed82
Christian Balidoy <redchristian20@gmail.com>
2025-06-18 01:36:40 +0800

    .

---
+++ alchemy/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Partners from './components/Partners/Partners'
+import Services from './components/Services/Services'
+import Footer from './components/Footer/Footer'
+function App() {
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <Partners/>
+      <Services/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
